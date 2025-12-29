62fcded7a79a37a3f9351e40864773db743be5dc
3-shravan <shantanuyadav379@gmail.com>
2025-12-29 14:40:21 +0530

    [Sravan Kumar] Add (us6). calculating the wage until the limit.

--- reviews/EmployeeWagerComputation.java
+++ reviews/EmployeeWagerComputation.java
@@ -5,7 +5,8 @@ public class EmployeeWagerComputation {
   private static final int WAGE_PER_HOUR = 20;
   private static final int FULL_TIME_HOUR = 8;
   private static final int PART_TIME_HOUR = 8;
-   private static final int WORKING_DAYS = 20;
+  private static final int WORKING_DAYS = 20;
+  private static final int MAX_HOURS = 100;
 
   public static void main(String[] args) {
     System.out.println("Welcom to Emplyee Wage Computation");
@@ -24,8 +25,11 @@ public class EmployeeWagerComputation {
     int wage = calculateWage(hours);
     System.out.println("Wage for selected employee type: " + wage);
 
-    int monthlyWages=calculateMonthlyWage();
+    int monthlyWages = calculateMonthlyWage();
     System.out.println("Monthly wages:" + monthlyWages);
+    
+    int wageWithLimits=calculateWageUntilLimit();
+    System.out.println("wages until limit:" + wageWithLimits);
     
 
   }
@@ -62,6 +66,22 @@ public class EmployeeWagerComputation {
     for (int day = 1; day <= WORKING_DAYS; day++) {
       totalWage += calculateDailyWage();
     }
+    return totalWage;
+  }
+
+  static int calculateWageUntilLimit() {
+    int totalWage = 0;
+    int totalHours = 0;
+    int days = 0;
+
+    while (totalHours < MAX_HOURS && days < WORKING_DAYS) {
+      if (isEmployeePresent()) {
+        totalHours += FULL_TIME_HOUR;
+        totalWage += WAGE_PER_HOUR * FULL_TIME_HOUR;
+      }
+      days++;
+    }
+
     return totalWage;
   }
 

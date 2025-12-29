d204f0db7e0c64818cb6469b29de914feb817fa7
3-shravan <shantanuyadav379@gmail.com>
2025-12-29 14:29:21 +0530

    [Sravan Kumar] Add (uc4). solving using the switch case statements

--- reviews/EmployeeWagerComputation.java
+++ reviews/EmployeeWagerComputation.java
@@ -1,51 +1,55 @@
 import java.util.Scanner;
 
 public class EmployeeWagerComputation {
-  
-    private static final int WAGE_PER_HOUR = 20;
-    private static final int FULL_TIME_HOUR = 8;
-    private static final int PART_TIME_HOUR=8;
-    
+
+  private static final int WAGE_PER_HOUR = 20;
+  private static final int FULL_TIME_HOUR = 8;
+  private static final int PART_TIME_HOUR = 8;
+
   public static void main(String[] args) {
     System.out.println("Welcom to Emplyee Wage Computation");
 
-   int dailyWage = calculateDailyWage();
+    int dailyWage = calculateDailyWage();
     System.out.println("Daily Wage: " + dailyWage);
 
     System.out.println("Employee type: ");
     System.out.println("1. part time ");
     System.out.println("2. full time ");
-    
-Scanner sc = new Scanner (System.in);
-int employeeType = sc.nextInt();
 
-if(employeeType == 1){
-  int wage = calculateWage(PART_TIME_HOUR);
-  System.out.println("Part-time Wage: " + wage);
-} else if(employeeType == 2){
-  int wage = calculateWage(FULL_TIME_HOUR);
-  System.out.println("Full-time Wage: " + wage);
-}
+    Scanner sc = new Scanner(System.in);
+    int employeeType = sc.nextInt();
+
+    int hours = getHours(employeeType);
+    int wage = calculateWage(hours);
+    System.out.println("Wage for selected employee type: " + wage);
 
   }
 
-private static boolean isEmployeePresent() {
-  int attendance = (int) (Math.random() * 2);
-  return attendance == 1;
-}
+  private static boolean isEmployeePresent() {
+    int attendance = (int) (Math.random() * 2);
+    return attendance == 1;
+  }
 
-static int calculateDailyWage() {
-  if (isEmployeePresent()) {
+  static int calculateDailyWage() {
+    if (isEmployeePresent()) {
       return WAGE_PER_HOUR * FULL_TIME_HOUR;
     }
     return 0;
   }
- 
 
-    static int calculateWage(int hours) {
+  static int calculateWage(int hours) {
     return WAGE_PER_HOUR * hours;
   }
 
-   
+  static int getHours(int empType) {
+    switch (empType) {
+      case 1:
+        return FULL_TIME_HOUR;
+      case 2:
+        return PART_TIME_HOUR;
+      default:
+        return 0;
+    }
+  }
 
 }

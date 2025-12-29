5be7866f6d4f302aba7dedddb1a5d4940fd8e78e
3-shravan <shantanuyadav379@gmail.com>
2025-12-29 12:50:44 +0530

    [Sravan Kumar] Add (uc2). calculate daily emplyee wage

--- reviews/EmpoyeeWagerComputation.java
+++ reviews/EmpoyeeWagerComputation.java
@@ -1,4 +1,7 @@
 public class EmpoyeeWagerComputation {
+
+  private static final int WAGE_PER_HOUR = 20;
+  private static final int FULL_TIME_HOUR = 8;
   
 }
 public class EmployeeWagerProblem {
@@ -6,14 +9,21 @@ public class EmployeeWagerProblem {
   public static void main(String[] args) {
     System.out.println("Welcom to Emplyee Wage Computation");
 
-    boolean isPresent = isEmployeeAbsentPresent();
-    System.out.println(isPresent);
+   int dailyWage = calculateDailyWage();
+    System.out.println("Daily Wage: " + dailyWage);
 
   }
 
   private static boolean isEmployeeAbsentPresent() {
     int attendance = (int) (Math.random() * 2);
     return attendance == 1;
+  }
+
+ static int calculateDailyWage() {
+    if (isEmployeePresent()) {
+      return WAGE_PER_HOUR * FULL_TIME_HOUR;
+    }
+    return 0;
   }
 
 }

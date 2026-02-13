8791e71648d3227281977576d351e757199de8e1
3-shravan <shantanuyadav379@gmail.com>
2026-02-13 23:43:56 +0530

    [Sravan Kumar] Add. j unit scenario based testing problem

---
+++ collections-practice/gcr-codebase/junit/src/test/java/com/shravan/junit_scenario_based_Problem/Program.java
@@ -0,0 +1,24 @@
+package com.shravan.junit_scenario_based_Problem;
+
+public class Program {
+
+  public Program (double amount){
+    
+  }
+
+  public void Deposit(double d) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'Deposit'");
+  }
+
+  public Double getBalance() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getBalance'");
+  }
+
+  public void Withdraw(double d) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'Withdraw'");
+  }
+
+}
